7585a96a082ddc2936d41fd63747afea0f5c6760
Patricia Carrasco <patri.carrasco@gmail.com>
2022-01-02 14:13:38 +0100

    modi enemy

--- src/actors/Enemy.ts
+++ src/actors/Enemy.ts
@@ -4,10 +4,12 @@ import { Point } from "../types/Point";
 import { checkLimits } from "../utils/CheckLimits";
 //import { CanyonKey, KeyboardMap } from "../utils/KeyboardMap";
 import { convertToObject } from "typescript";
-import { directionEnemy } from "../utils/MoveEnemy";
+
 //const ferrariImg = require("../assets/ferrari.png");
 
 type Size = { w: number; h: number };
+// Aquí podemos cambiar la velocidad del enemigo.
+let speedEnemy = 6;
 
 export class Enemy extends Actor implements IActor {
 	enemySize: Size;
@@ -25,7 +27,7 @@ export class Enemy extends Actor implements IActor {
 		this.enemySize = size;
 		this.enemyColor = "red";
 		this.direction= 1;
-		this.enemySpeed = 1.5*this.direction; 
+		this.enemySpeed = speedEnemy*this.direction; 
 		// // Car image
 		// this.image = new Image();
 		// this.image.src = ferrariImg;
@@ -36,20 +38,15 @@ export class Enemy extends Actor implements IActor {
 			x: this.position.x + this.enemySpeed,
 			y: this.position.y,
 		};
-		console.log(this.position.x)   
+		
 		if (checkLimits(newPos,this.enemySize.w,sizeCanvas)) {
 			this.position = newPos;	 
 			        
 		}
-		// this.direction = directionEnemy(this.position)
-		// console.log("enemy",this.direction)
-		// this.enemySpeed = 1.5*this.direction;
-		// console.log(this.enemySpeed,this.position.x)
+		if(this.position.x >=sizeCanvas-this.enemySize.w- this.enemySpeed || this.position.x <=1){
 		
-
-		 if(this.position.x >968 || this.position.x <=1){
-			this.direction = this.position.x >968 ? -1 : 1
-			this.enemySpeed = 1.5*this.direction;
+			this.direction = this.position.x >=sizeCanvas-this.enemySize.w-this.enemySpeed  ? -1 : 1
+			this.enemySpeed = speedEnemy*this.direction;
 			this.position.y+=50;
 		}
 	}
